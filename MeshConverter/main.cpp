28ed0afdc5d0d81eea9615f35f03196d82e8e3ef
Anton Surkis <antonsurkis@gmail.com>
2024-02-05 15:40:44 +0300

    Initial mesh load

---
+++ MeshConverter/main.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include "tiny_gltf.h"
+
+int main()
+{
+    tinygltf::Model model;
+    tinygltf::TinyGLTF ctx;
+    std::string err;
+    std::string warn;
+    bool success = ctx.LoadBinaryFromFile(&model, &err, &warn, "input.glb");
+    std::cout << success << '\n' << err << '\n' << warn << std::endl;
+}
